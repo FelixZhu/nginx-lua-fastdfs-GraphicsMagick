49a3157dd3f9d18201254bd0c27a01ed098b3c2e
felixzhu <felixzhu@sohu-inc.com>
2014-08-26 17:06:33 +0800

    deho

--- lua/fastdfs.lua
+++ lua/fastdfs.lua
@@ -25,26 +25,29 @@ end
 
 -- 查找是否有图片大小的后缀
 local area = nil
-local originalUri = ngx.var.uri;
+-- 从ngx.var获取参数的时候最好赋值到本地变量，否则会重复分配内存，在请求结束的时候才会释放
+local originalUri = ngx.var.uri; -- http://192.168.110.218/g1/M00/00/33/CgoYvFP0RU2AfE_wAA5UF3DBJeE494.jpg_60x60.jpg
 local originalFile = ngx.var.file;
+local index = string.find(ngx.var.uri, "?");
 local index = string.find(ngx.var.uri, "([0-9]+)x([0-9]+)");
+
 if index then
-    originalUri = string.sub(ngx.var.uri, 0, index-2);
+    originalUri = string.sub(ngx.var.uri, 0, index-2); -- http://192.168.110.218/g1/M00/00/33/CgoYvFP0RU2AfE_wAA5UF3DBJeE494.jpg
     area = string.sub(ngx.var.uri, index);
     index = string.find(area, "([.])");
-    area = string.sub(area, 0, index-1);
+    area = string.sub(area, 0, index-1); -- 60x60
 
     local index = string.find(originalFile, "([0-9]+)x([0-9]+)");
-    originalFile = string.sub(originalFile, 0, index-2)
+    originalFile = string.sub(originalFile, 0, index-2); -- /opt/data/fastdfs/storage/data/
 end
 
 -- 如果文件不存在，从tracker下载
 if not file_exists(originalFile) then
-    local fileid = string.sub(originalUri, 2);
+    local fileid = string.sub(originalUri, 2)
     local fastdfs = require('restyfastdfs')
 
     local fdfs = fastdfs:new()
-    fdfs:set_tracker("192.168.110.218", 22122)
+    fdfs:set_tracker(ngx.var.tracker_ip, 22122)
     fdfs:set_timeout(1000)
     fdfs:set_tracker_keepalive(0, 100)
     fdfs:set_storage_keepalive(0, 100)
@@ -70,8 +73,10 @@ end
 
 -- 如果在允许的缩略图大小中，创建缩略图
 -- 如果有了是否还会再转呢？？
+local gm = ngx.var.gm
+if not gm then gm = "gm" end
 if table.contains(image_sizes, area) then
-    local command = "/opt/apps/GraphicsMagick/bin/gm convert " .. originalFile  .. " -thumbnail " .. area .. " -background white -gravity center -extent " .. area .. " " .. ngx.var.file;
+    local command = ngx.var.gm .. " convert " .. originalFile  .. " -thumbnail " .. area .. " -background white -gravity center -extent " .. area .. " " .. ngx.var.file;
     os.execute(command);
 end;
 
