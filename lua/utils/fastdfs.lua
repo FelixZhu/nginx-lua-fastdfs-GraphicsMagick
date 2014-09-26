ff5b43a6aa21579529dcd39e25f3dce058b0d9ac
felixzhu <felixzhu@sohu-inc.com>
2014-09-26 18:30:01 +0800

    哈哈哈哈滚你妹的

---
+++ lua/utils/fastdfs.lua
@@ -0,0 +1,39 @@
+local tracker = require('resty.fastdfs.tracker')
+local storage = require('resty.fastdfs.storage')
+
+function _dump_res(res)
+    for i in pairs(res) do
+        ngx.say(string.format("%s:%s",i, res[i]))
+        ngx.log(ngx.ERR, string.format("%s:%s",i, res[i]))
+    end
+    ngx.say("")
+end
+
+local fileid = "g1/M00/00/00/CgoYr1QIOgSAF6vPAAENdjraJ243824185"
+
+local tk = tracker:new()
+tk:set_timeout(3000)
+local ok, err = tk:connect({host='192.168.110.218', port=22122})
+
+
+if not ok then
+    ngx.say('connect error:' .. err)
+    ngx.exit(200)
+end
+
+local res, err = tk:query_storage_store()
+if not res then
+    ngx.say("query storage error:" .. err)
+    ngx.exit(200)
+end
+
+
+
+local res, err = tk:query_storage_fetch1(fileid)
+if not res then
+    ngx.say("query storage error:" .. err)
+    ngx.exit(200)
+else
+    _dump_res(res)
+    ngx.exit(200)
+end
---
+++ lua/utils/tools.lua
@@ -0,0 +1,35 @@
+
+-- 写入文件
+local function writefile(filename, info)
+    local wfile=io.open(filename, "w") --写入文件(w覆盖)
+    assert(wfile)  --打开时验证是否出错
+    wfile:write(info)  --写入传入的内容
+    wfile:close()  --调用结束后记得关闭
+end
+
+-- 检测路径是否目录
+local function is_dir(sPath)
+    if type(sPath) ~= "string" then return false end
+
+    local response = os.execute( "cd " .. sPath )
+    if response == 0 then
+        return true
+    end
+    return false
+end
+
+-- 检测文件是否存在
+local file_exists = function(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+end
+
+-- 检测table是否包含某个值
+function table.contains(table, element)
+    for _, value in pairs(table) do
+        if value == element then
+            return true
+        end
+    end
+    return false
+end
